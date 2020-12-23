a12ad99fa45ef761198d7f1ee8d36ab8162c9975
Abayomi <eyitouyoa3@gmail.com>
2020-12-23 11:24:59 -0500

    Home page with background image

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <section id="Home">
+            <div className="bg-image"></div>
+            <div className="bg-darken"></div>
+        </section>
+    )
+}
+
+export default Home
