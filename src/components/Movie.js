bf252d000ba53fc4117bd0a5151d559ba506b500
Abayomi <eyitouyoa3@gmail.com>
2020-12-24 15:29:10 -0500

    User can nominate movies and fill up nomination slots.

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useRef } from 'react'
 
-const Movie = ({ name, year, poster }) => {
+const Movie = ({ name, year, poster, addToNomination, id, btnText }) => {
+
+    const buttonRef = useRef();
+
+    const handleClick = (movie) => {
+        addToNomination(movie)
+        buttonRef.current.disabled = true;
+    }
+
     return (
-        <li>
-            {name} ({year})
-            <img src={poster} alt="" />
-        </li>
+        <div className="card">
+            <img src={poster} alt="Movie Poster" />
+            <div className="movie-blurb">
+                <h4>{name} ({year})</h4>
+                <button ref={buttonRef} onClick={() => handleClick({ name, year, poster, id })}>{btnText}</button>
+            </div>
+        </div>
     )
 }
 
--- src/components/SearchMovie.js
+++ src/components/SearchMovie.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { connect } from 'react-redux';
 import { getMovies } from '../actions/movieActions'
 import Movie from './Movie';
@@ -7,21 +7,43 @@ const SearchMovie = ({ getMovies, movies }) => {
 
     const [movieName, setMoviename] = useState('');
     const [movieList, setMovieList] = useState([]);
+    const [nominationList, setNominationList] = useState([]);
+
+    const popup = useRef();
+
+    const addToNomination = (movie) => {
+        if (nominationList.length + 1 > 5)
+            popup.current.classList.toggle("active");
+
+        else
+            setNominationList([...nominationList, movie])
+
+    }
 
     useEffect(() => {
         getMovies(movieName);
     }, [getMovies, movieName])
 
-    useEffect(() => {
-        const movieArray = movies ? movies.map(movie => {
-            return (<Movie name={movie.Title} year={movie.Year} poster={movie.Poster} key={movie.imdbID} />)
-        }) : (<p>Loading movies...</p>);
 
-        setMovieList(movieArray)
+    useEffect(() => {
+        setMovieList(movies);
     }, [movies])
+
+    const movieArray = movieList ? movieList.map(movie => {
+        return (<Movie name={movie.Title}
+            year={movie.Year}
+            poster={movie.Poster}
+            addToNomination={addToNomination}
+            btnText="Nominate"
+            id={movie.imdbID}
+            key={movie.imdbID} />)
+    }) : (<p>Nothing to see here yet...</p>);
 
     return (
         <>
+            <div ref={popup} className="popup">
+                <p>You cannot have more than 5 movies in your nominations.</p>
+            </div>
             <h1 className="main-header">The Shoppies</h1>
             <div className="container">
                 <form>
@@ -33,13 +55,20 @@ const SearchMovie = ({ getMovies, movies }) => {
                 <div className="movie-list">
                     <div className="results">
                         <h2>Results for "{movieName}"</h2>
-                        <ul>
-                            {movieList}
-                        </ul>
+                        <div className="results-list">
+                            {movieArray}
+                        </div>
                     </div>
 
-                    <div className="nominations">
+                    <div className="results">
                         <h2>Nominations</h2>
+                        {nominationList.map((nomination) => {
+                            return (<Movie name={nomination.name}
+                                year={nomination.year}
+                                poster={nomination.poster}
+                                btnText="Remove"
+                                key={nomination.id} />)
+                        })}
                     </div>
 
                 </div>
